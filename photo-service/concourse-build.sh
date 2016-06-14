f3035f8d4bec34389a4e9806223cd1501c6fe92b
Ravi Jagannathan <ravijagannathan@Ravis-MacBook-Pro.local>
2016-06-14 15:28:43 -0400

    V1.7 Concourse build

--- photo-service/concourse-build.sh
+++ photo-service/concourse-build.sh
@@ -3,20 +3,20 @@
 set -e # fail fast
 set -x # print commands
 
-git clone <source> <target>
-
-cd updated-gist
-ls -la
-echo $(date) > bumpme
-
-git config --global user.email "reagul@concourse.ci"
-git config --global user.name "reagul"
-
-git add .
-git commit -m "Bumped date"
-ls -la
-pwd
-cd ../
+# git clone <source> <target>
+#
+# cd <target>
+# ls -la
+# echo $(date) > bumpme
+#
+# git config --global user.email "reagul@concourse.ci"
+# git config --global user.name "reagul"
+# 
+# git add .
+# git commit -m "Bumped date"
+# ls -la
+# pwd
+# cd ../
 
 echo "test message here"
 ls -la
